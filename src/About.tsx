3880c1cd03e421f174b00fed47418efab2b2db8d
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2025-05-31 13:33:51 +0900

    a

--- src/About.tsx
+++ src/About.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>自己紹介ページ</h1>
-      <ul>
-        <li>名前：木村 颯汰</li>
+    <div className="shisha-container">
+      <div className="shisha-body">
+        <div className="shisha-base"></div>
+        <div className="shisha-neck"></div>
+      </div>
+      <div className="smoke">
+        <span className="smoke-text">Sota Kimura</span>
+      </div>
+      <ul className="about-list">
         <li>趣味：バイク、シーシャ、酒</li>
         <li>経歴：2022年からエンジニア学生してます</li>
         <li>今年の目標: バイク2台購入</li>
